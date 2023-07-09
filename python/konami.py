aa91107bce6620a0c73ce35727ab5e1b2177177e
Christian <aqchristian12@gmail.com>
2023-07-09 16:57:22 +0200

    change tabs

---
+++ python/konami.py
@@ -0,0 +1,13 @@
+from pynput.keyboard import Key, KeyCode, Listener
+
+KONAMI_CODE = [
+    Key.up, Key.up, Key.down, Key.down,
+    Key.left, Key.right, Key.left, Key.right,
+    KeyCode.from_char("b"), KeyCode.from_char("a")
+]
+
+def on_press(key) :
+    print(key)
+
+with Listener(on_press=on_press) as listener:
+    listener.join()
