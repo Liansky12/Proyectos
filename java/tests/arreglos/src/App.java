83d6c2da1bfe0683c4c20611aab2a2bda02f0380
Christian <aqchristian12@gmail.com>
2023-08-08 21:33:28 +0200

    feat tests in java proyects

---
+++ java/tests/arreglos/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        final int[] arr = { 1, 2 };
+        arr[0] = 3;
+        System.out.println("arr[0]= " + arr[0]);
+    }
+}
